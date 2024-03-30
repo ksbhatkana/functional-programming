d2f599e54ad43fe5e4b93ecd0cd61afbc14bc903
KS Bhat <kumarasubrahmanya.h@gmail.com>
2024-03-30 11:15:30 +0530

    More on FP

--- src/main/java/com/ksbhat/microservices/functionalprogramming/PlayWithOptional.java
+++ src/main/java/com/ksbhat/microservices/functionalprogramming/PlayWithOptional.java
@@ -1,5 +1,6 @@
 package com.ksbhat.microservices.functionalprogramming;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -8,6 +9,9 @@ public class PlayWithOptional {
         List<String> fruits = List.of("apple", "banana", "orange");
 
         Predicate<? super String> predicate = fruit -> fruit.startsWith("b");
+
+        List<String> courseList = List.of("Java", "Python", "C++");
+        courseList.stream().sorted(Comparator.comparing(str -> str.length())).forEach(System.out::println);
         
     }
 }
--- src/main/java/com/ksbhat/microservices/functionalprogramming/PrintAllNumbersFP.java
+++ src/main/java/com/ksbhat/microservices/functionalprogramming/PrintAllNumbersFP.java
@@ -3,7 +3,13 @@ package com.ksbhat.microservices.functionalprogramming;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 @Component
 @Scope(value = "prototype")
@@ -31,4 +37,92 @@ public class PrintAllNumbersFP {
         courseList.stream().map(course -> course + " " + course.length()).forEach(System.out::println);
     }
 
+    public static void printSumOfListFP() {
+        List<Integer> courseList = List.of(1, 2, 3, 4, 5, 6);
+        System.out.println(courseList.stream().reduce(0, (aggregate, nextNumber) -> aggregate + nextNumber));
+    }
+
+    public static void printSumOfListFP2ndApproach() {
+        List<Integer> courseList = List.of(1, 2, 3, 4, 5, 6);
+        System.out.println(courseList.stream().reduce(0, Integer::sum));
+    }
+
+    public static void findMaxValue() {
+        List<Integer> courseList = List.of(1, 2, 3, 4, 5, 6);
+        System.out.println(courseList.stream()
+                .reduce(Integer.MIN_VALUE, (previousMax, currentNumber) ->
+                        currentNumber > previousMax ? currentNumber:previousMax ));
+    }
+
+    public static void printDistinct() {
+        List<Integer> courseList = List.of(1, 2, 3, 4, 5, 6, 4, 1);
+        courseList.stream().distinct().forEach(System.out::println);
+    }
+
+    public static void printSorted() {
+        List<Integer> courseList = List.of(1, 2, 3, 4, 5, 6, 4, 1);
+        courseList.stream().sorted().forEach(System.out::println);
+    }
+
+    public static void printListOfStringsInOrder() {
+        List<String> courseList = List.of("Java", "Python", "C++");
+        courseList.stream().sorted(Comparator.naturalOrder()).forEach(System.out::println);
+    }
+
+    public static void printListOfStringsInDescOrder() {
+        List<String> courseList = List.of("Java", "Python", "C++");
+        courseList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+    }
+
+    public static void printListOfStringsInOrderOfLength() {
+        List<String> courseList = List.of("Java", "Python", "C++");
+        courseList.stream().sorted(Comparator.comparing(str -> str.length())).forEach(System.out::println);
+    }
+
+    public static void functionalInterfacesExample() {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 4, 1);
+        Predicate<Integer> isEvenPredicate = number -> number %2 == 0;
+        Predicate<Integer> isEvenPredicate2 = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer number) {
+                return number %2 == 0;
+            }
+        };
+
+        Function<Integer, Integer> returnSquare = number -> number * number;
+        Function<Integer, Integer> returnSquare2 = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer number) {
+                return number * number;
+            }
+        };
+
+        Consumer<Integer> printConsumer = System.out::println;
+        Consumer<Integer> printConsumer2 = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+
+        BinaryOperator<Integer> sumTwoIntegers = Integer::sum;
+        BinaryOperator<Integer> sumTwoIntegers2 = new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        };
+
+        List<Integer> numbers = List.of(1, 2, 3, 4);
+        filterAndPrint(numbers, number -> number % 2 == 0);
+        filterAndPrint(numbers, number -> number % 2 != 0);
+
+        integers.stream().filter(isEvenPredicate).forEach(System.out::println);
+    }
+
+    private static void filterAndPrint(List<Integer> numbers, Predicate<Integer> predicate) {
+        numbers.stream()
+                .filter(predicate)
+                .forEach(System.out::println);
+    }
 }
